820d03a55c0d2befd68672264b27e2814fa707ed
James Ward <james.jward@hotmail.com>
2022-05-27 14:01:09 -0300

    Added ethswap contract templet and generated the contract json

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,8 @@
-require("babel-register");
-require("babel-polyfill");
-
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*",
     },
   },
